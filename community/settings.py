5f82da09d90715dc7537565bb0ca92dbb3872ea3
Harshith <mailtoharshiths@gmail.com>
2024-06-13 19:31:22 +0530

    Commit for deployment

--- community/settings.py
+++ community/settings.py
@@ -24,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = 'django-insecure-^c3l_vs@#h85*q&0auzsgxwi!7w5x&5s*oo)re$pa!m#vmqtbu'
+DEBUG=True
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
